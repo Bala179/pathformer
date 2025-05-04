8b6c780dae64d91993126c07f1ca3ac06fdfd5f7
Balaganesh Manikandan <mbala82001@gmail.com>
2025-05-03 23:17:46 -0400

    Added scripts for cluster_5 and cluster_7

--- scripts/multivariate/retail.sh
+++ scripts/multivariate/retail.sh
@@ -13,41 +13,13 @@ model_id_name=retail
 data_name=custom
 
 pred_len=10
-# for data_path_name in cluster_1.csv cluster_5.csv cluster_7.csv
-# do
-#     python -u run.py \
-#       --is_training 1 \
-#       --root_path $root_path_name \
-#       --data_path $data_path_name \
-#       --model_id $model_id_name_$seq_len'_'$pred_len \
-#       --model $model_name \
-#       --data $data_name \
-#       --features MS \
-#       --freq d \
-#       --seq_len $seq_len \
-#       --pred_len $pred_len \
-#       --num_nodes 321 \
-#       --layer_nums 3 \
-#       --residual_connection 1\
-#       --k 2\
-#       --d_model 16 \
-#       --d_ff 128 \
-#       --patch_size_list 16 12 8 32 12 8 6 4 8 6 4 2 \
-#       --metric mape \
-#       --train_epochs 5\
-#       --patience 10 \
-#       --lradj 'TST' \
-#       --pct_start 0.2 \
-#       --itr 1 \
-#       --batch_size 16 --learning_rate 0.001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
-# done
 
 data_path_name=cluster_1.csv
 python -u run.py \
     --is_training 1 \
     --root_path $root_path_name \
     --data_path $data_path_name \
-    --model_id $model_id_name_$seq_len'_'$pred_len \
+    --model_id $model_id_name_$data_path_name'_'$seq_len'_'$pred_len \
     --model $model_name \
     --data $data_name \
     --features MS \
@@ -62,10 +34,63 @@ python -u run.py \
     --d_ff 128 \
     --patch_size_list 12 6 4 6 4 3 6 4 12 2 4 2 \
     --metric mape \
-    --train_epochs 5\
+    --train_epochs 20 \
     --patience 10 \
     --lradj 'TST' \
     --pct_start 0.2 \
     --itr 1 \
-    --batch_size 16 --learning_rate 0.001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+    --batch_size 16 --learning_rate 0.001 >logs/LongForecasting/$model_name'_'$data_path_name'_'$seq_len'_'$pred_len.log
 
+data_path_name=cluster_5.csv
+python -u run.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name_$data_path_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features MS \
+    --freq d \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --num_nodes 1944 \
+    --layer_nums 3 \
+    --residual_connection 1\
+    --k 2\
+    --d_model 16 \
+    --d_ff 128 \
+    --patch_size_list 12 6 4 6 4 3 6 4 12 2 4 2 \
+    --metric mape \
+    --train_epochs 20\
+    --patience 10 \
+    --lradj 'TST' \
+    --pct_start 0.2 \
+    --itr 1 \
+    --batch_size 16 --learning_rate 0.001 >logs/LongForecasting/$model_name'_'$data_path_name'_'$seq_len'_'$pred_len.log
+
+data_path_name=cluster_7.csv
+python -u run.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name_$data_path_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features MS \
+    --freq d \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --num_nodes 145 \
+    --layer_nums 3 \
+    --residual_connection 1\
+    --k 2\
+    --d_model 16 \
+    --d_ff 128 \
+    --patch_size_list 12 6 4 6 4 3 6 4 12 2 4 2 \
+    --metric mape \
+    --train_epochs 20\
+    --patience 10 \
+    --lradj 'TST' \
+    --pct_start 0.2 \
+    --itr 1 \
+    --batch_size 16 --learning_rate 0.001 >logs/LongForecasting/$model_name'_'$data_path_name'_'$seq_len'_'$pred_len.log
